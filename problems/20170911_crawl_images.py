a158eb5a45a0daee246250e6a5d4b619bba8de4c
schubisu <robin.schubert@ghi-muenster.de>
2017-09-11 16:34:23 +0200

    updated file to take single image urls as well

--- problems/20170911_crawl_images.py
+++ problems/20170911_crawl_images.py
@@ -6,9 +6,18 @@ from bs4 import BeautifulSoup
 def save_all_images(url):
     soup = BeautifulSoup(requests.get(url).text, 'lxml')
     images = soup.find_all('img')
-    for image in images:
-        image_link = os.path.join(url, image['src'])
-        image_filename = os.path.basename(image_link)
-        image_raw = requests.get(image_link).content
-        with open(image_filename, 'bw') as outstream:
-            outstream.write(image_raw)
+    if images:
+        for image in images:
+            image_link = os.path.join(url, image['src'])
+            image_filename = os.path.basename(image_link)
+            image_raw = requests.get(image_link).content
+            with open(image_filename, 'bw') as outstream:
+                outstream.write(image_raw)
+    else:
+        try:
+            image_filename = os.path.basename(url)
+            image_raw = requests.get(url).content
+            with open(image_filename, 'bw') as outstream:
+                outstream.write(image_raw)
+        except:
+            print("no images found")
