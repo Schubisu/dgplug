650d18f55204b4db81934e1429b1970339a29f17
schubisu <robin.schubert@ghi-muenster.de>
2017-09-13 16:25:41 +0200

    added magic number verification

--- problems/20170911_crawl_images.py
+++ problems/20170911_crawl_images.py
@@ -3,6 +3,36 @@ import os
 from bs4 import BeautifulSoup
 
 
+magic_numbers = {
+    'png': ["89 50 4E 47 0D 0A 1A 0A"],
+    'jpg': [
+        "FF D8 FF DB",
+        "FF D8 FF E0",
+        "FF D8 FF E1",
+    ],
+}
+
+
+def check_image_integrity(image_data, image_name):
+    image_extension = os.path.splitext(image_name)[-1].strip('.')
+    image_signatures = magic_numbers.get(image_extension, [])
+    for signature in image_signatures:
+        signature_int = [int(i, 16) for i in signature.split(' ')]
+        if list(image_data[:len(signature_int)]) == signature_int:
+            return True
+
+    print("could not verify signature of {}".format(
+        image_name
+    ))
+    return False
+
+
+def save_image(image_data, image_filename):
+    if check_image_integrity(image_data, image_filename):
+        with open(image_filename, 'bw') as outstream:
+            outstream.write(image_data)
+
+
 def save_all_images(url):
     request = requests.get(url)
     soup = BeautifulSoup(request.text, 'lxml')
@@ -12,14 +42,17 @@ def save_all_images(url):
             image_link = os.path.join(url, image['src'])
             image_filename = os.path.basename(image_link)
             image_raw = requests.get(image_link).content
-            with open(image_filename, 'bw') as outstream:
-                outstream.write(image_raw)
+            save_image(image_raw, image_filename)
 
     elif 'image' in request.headers['Content-Type']:
         image_filename = os.path.basename(url)
         image_raw = requests.get(url).content
-        with open(image_filename, 'bw') as outstream:
-            outstream.write(image_raw)
+        save_image(image_raw, image_filename)
 
     else:
         print('no images found')
+
+
+if __name__ == "__main__":
+    url = "https://dgplug.org/assets/img/header.png"
+    save_all_images(url)
